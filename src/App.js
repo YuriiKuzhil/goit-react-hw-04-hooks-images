5f23c07ed84a06028b1be63aa898f25111237d12
Yurii <yuriikuzhil@gmail.com>
2022-01-17 23:03:03 +0200

    reworked files with use hooks

--- src/App.js
+++ src/App.js
@@ -1,45 +1,98 @@
-import React, { Component } from 'react';
-import { Toaster } from 'react-hot-toast';
+import { useState, useEffect } from 'react';
+import toast, { Toaster } from 'react-hot-toast';
 import './App.css';
 import Searchbar from './components/searchbar';
 import ImageGallery from './components/imageGallery';
 import Modal from './components/modal';
+import imageFinderApi from './services/imageFinderApi';
+import Loader from './components/loader';
+import ButtonLoadMore from './components/buttonLoadMore';
 
-class App extends Component {
-  state = {
-    imageName: '',
-    showModal: false,
-    modalContent: null,
+export default function App() {
+  const [imageName, setImageName] = useState('');
+  const [page, setPage] = useState(1);
+  const [fetchImages, setFetchImages] = useState([]);
+  const [showModal, setShowModal] = useState(false);
+  const [modalContent, setmodalContent] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [showButton, setShowButton] = useState(false);
+  const [itemToScroll, setItemToScroll] = useState(false);
+
+  useEffect(() => {
+    const getImages = async (imageName, page) => {
+      setIsLoading(true);
+      const data = await imageFinderApi(imageName, page);
+      try {
+        if (data.total === 0) {
+          toast.error('Images has not been found!');
+          setFetchImages([]);
+          setPage(1);
+          setShowButton(false);
+          return;
+        }
+
+        const quantityOfPage = data.total / 12;
+        setShowButton(quantityOfPage > page ? true : false);
+
+        if (page === 1) {
+          setFetchImages(data.hits);
+          setItemToScroll(null);
+          return;
+        } else {
+          setFetchImages(state => [...state, ...data.hits]);
+          setItemToScroll(data.hits[data.hits.length - 1].id);
+        }
+      } catch {
+        toast.error('Something wrong!');
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    if (!imageName) {
+      return;
+    }
+    getImages(imageName, page);
+  }, [imageName, page]);
+
+  const handleFormSubmit = imageName => {
+    setImageName(imageName);
+    setPage(1);
+    setFetchImages([]);
+  };
+  const toggleModal = () => {
+    setShowModal(state => !state);
+  };
+  const getModalContent = modalContent => {
+    setmodalContent(modalContent);
+  };
+  const loadMoreImages = () => {
+    setPage(state => state + 1);
+    setShowButton(false);
   };
 
-  handleFormSubmit = imageName => {
-    this.setState({ imageName });
-  };
+  document.getElementById(itemToScroll)?.scrollIntoView({
+    behavior: 'smooth',
+    block: 'start',
+  });
 
-  toggleModal = () => {
-    this.setState(({ showModal }) => ({
-      showModal: !showModal,
-    }));
-  };
-  getModalContent = modalContent => {
-    this.setState({ modalContent });
-  };
-  render() {
-    const { showModal, modalContent, imageName } = this.state;
-
-    return (
-      <div className="App">
-        <Searchbar onSubmit={this.handleFormSubmit} />
+  return (
+    <div className="App">
+      <Searchbar onSubmit={handleFormSubmit} />
+      {fetchImages.length > 0 && (
         <ImageGallery
-          getModalContent={this.getModalContent}
-          imageName={imageName}
-          openModal={this.toggleModal}
+          getModalContent={getModalContent}
+          fetchImages={fetchImages}
+          toggleModal={toggleModal}
         />
-        <Toaster position="bottom-right" />
-        {showModal && <Modal onClose={this.toggleModal} data={modalContent} />}
-      </div>
-    );
-  }
-}
+      )}
 
-export default App;
+      {isLoading ? (
+        <Loader />
+      ) : (
+        showButton && <ButtonLoadMore onloadMoreImages={loadMoreImages} />
+      )}
+      <Toaster position="bottom-right" />
+      {showModal && <Modal onClose={toggleModal} data={modalContent} />}
+    </div>
+  );
+}
